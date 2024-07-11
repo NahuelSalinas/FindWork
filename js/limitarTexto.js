0173cdf37c2343b6c102a9f199be3c15d6884420
Nahuel Salinas <90853818+NahuelSalinas@users.noreply.github.com>
2024-07-11 13:55:48 -0300

    Fixeo de tarjetas

--- js/limitarTexto.js
+++ js/limitarTexto.js
@@ -1,12 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var titulos = document.querySelectorAll('.titulo-publicacion');
+    const longitudTitulos = 28; // Longitud máxima para los títulos
+    var parrafos = document.querySelectorAll('.descripcion-publicacion');
+    const longitudMaxima = 85; // Longitud máxima para las descripciones
 
+    // Limitar longitud de los títulos
+    titulos.forEach(function(titulo) {
+        var texto = titulo.textContent || titulo.innerText;
 
-var parrafos = document.getElementsByClassName('descripcion-publicacion');
-var longitudMaxima = 85;
+        if (texto.length > longitudTitulos) {
+            titulo.textContent = texto.substring(0, longitudTitulos) + '...';
+        }
+    });
 
-for (var i = 0; i < parrafos.length; i++) {
-    var texto = parrafos[i].innerHTML;
+    // Limitar longitud de las descripciones
+    parrafos.forEach(function(parrafo) {
+        var texto = parrafo.textContent || parrafo.innerText;
 
-    if (texto.length > longitudMaxima) {
-    parrafos[i].innerHTML = texto.substring(0, longitudMaxima) + '...';
-    }
-}
+        if (texto.length > longitudMaxima) {
+            parrafo.textContent = texto.substring(0, longitudMaxima) + '...';
+        }
+    });
+});
